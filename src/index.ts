0b16f1a6eb51c103543b04721fb7929e85936cc4
Zack <lorenx@163.com>
2025-11-20 10:51:52 +0800

    feat: initial i18n

---
+++ src/index.ts
@@ -0,0 +1,9 @@
+export { i18nPlugin } from './core/plugin'
+export { i18nPlugin as default } from './core/plugin'
+
+export type {
+  I18nPluginOptions,
+  ResolvedI18nPluginOptions,
+  LocaleMessages,
+  I18nContext,
+} from './types'
---
+++ tsdown.config.ts
@@ -0,0 +1,21 @@
+import { defineConfig } from 'tsdown'
+
+export default defineConfig([
+  {
+    entry: ['src/index.ts'],
+    format: ['cjs', 'esm'],
+    dts: true,
+    clean: true,
+    sourcemap: true,
+    external: ['vite'],
+    exports: 'named',
+  },
+  {
+    entry: ['src/runtime/index.ts'],
+    outDir: 'dist/runtime',
+    format: ['cjs', 'esm'],
+    dts: true,
+    sourcemap: true,
+    exports: 'named',
+  },
+])
